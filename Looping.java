e4f1cb477f74ec29b3162ce3c106fc86380f8f40
sgx-saksham <66645082+sgx-saksham@users.noreply.github.com>
2021-01-16 19:59:21 +0530

    Loops concept in Java

---
+++ Looping.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Looping {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.printf("%d x %d = %d %n", n, i, n * i);
+        }
+
+    }
+}
